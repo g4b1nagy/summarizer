ddc631c784a31e8a5c7c28e2845258bc97eb3b91
Gabi Nagy <gabrian.nagy@gmail.com>
2021-11-13 16:11:23 +0100

    Add DB model

--- README.md
+++ README.md
@@ -10,5 +10,6 @@ Dead simple summarization API
 * python3 -m venv venv
 * source venv/bin/activate
 * pip install -r requirements.txt
+* python -c 'from summarizer import db;db.create_all()'
 * FLASK_APP=summarizer FLASK_ENV=development flask run
 * go to http://localhost:5000/
--- summarizer.py
+++ summarizer.py
@@ -1,17 +1,48 @@
-from flask import Flask
+import datetime
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# =========================================================================
+# Setup and configuration
+# =========================================================================
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
+# Only needed if you need to get notified before and after changes are committed to the database.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# =========================================================================
+# Models
+# =========================================================================
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    updated = db.Column(db.DateTime, onupdate=datetime.datetime.utcnow)
+    text = db.Column(db.Text, nullable=False)
+    summary = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Document {self.id}>'
+
+# =========================================================================
+# Health check route
+# =========================================================================
 
 @app.route('/')
 def hello_world():
     return '<p>Hello, World!</p>'
 
+# =========================================================================
+# RESTful API document routes
+# =========================================================================
 
 # This, of course, is a very verbose and naive way of building the routes.
 # Something more structured, such as Flask-RESTful, would be preferable.
-
 
 @app.route('/document/<int:document_id>', methods=['GET'])
 def document_get(document_id):
